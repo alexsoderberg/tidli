1261d4009e133fd90117262a9b88df628bf57c0b
Alex Söderberg <as228gc@student.lnu.se>
2026-01-24 11:36:03 +0100

    Initial commit

---
+++ project_service/models/Project.cs
@@ -0,0 +1,9 @@
+namespace project_service.models;
+
+public class Project
+{
+  private readonly Guid id;
+  private TimeSpan TotalTime {get; set;} = TimeSpan.Zero;
+
+  
+}
---
+++ project_service/repository/IRepository.cs
@@ -0,0 +1,5 @@
+namespace project_service.repository;
+
+public interface IRepository
+{
+}
