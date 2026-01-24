b23c2600fd6c9b08752c331db19a0ef8f89935c7
Alex Söderberg <as228gc@student.lnu.se>
2026-01-24 14:24:34 +0100

    Add required to Title field

--- apps/project-service/models/ProjectTask.cs
+++ apps/project-service/models/ProjectTask.cs
@@ -3,8 +3,8 @@ namespace project_service.models;
 public class ProjectTask
 {
   public Guid Id { get; init; } = Guid.NewGuid();
-  public string Title { get; set; }
   public Guid ColumnId { get; set; }
+  public required string Title { get; set; }
   public TimeSpan TotalTime { get; set; } = TimeSpan.Zero;
   public DateTime? TimerStartedAt { get; set; }
   public bool IsTimerRunning => TimerStartedAt.HasValue;
