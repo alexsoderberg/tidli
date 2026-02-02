faeb5895e4901bf6b547bacf931a0d6f6ccb6a5c
Alex Söderberg <as228gc@student.lnu.se>
2026-02-02 17:40:45 +0100

    Test: Add test for GetTaskAsync

--- apps/project-service/tests/ProjectService.Tests/UnitTests/TaskServiceTests.cs
+++ apps/project-service/tests/ProjectService.Tests/UnitTests/TaskServiceTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using project_service.Data;
+using project_service.Dtos;
+using project_service.Models;
 using project_service.Services;
 
 namespace ProjectService.Tests.UnitTests;
@@ -21,12 +23,54 @@ public class TaskServiceTests : IDisposable
   }
 
   [Fact]
-  public void CreateTaskAsync_ShouldReturnTask_WhenValidParameters()
+  public async Task CreateTaskAsync_ShouldReturnTask_WhenValidParameters()
   {
     // Given
+    var column = new Column
+    {
+      Name = "column1",
+      ProjectId = Guid.NewGuid(),
+    };
+
+    var columnId = column.Id;
+
+    _context.Columns.Add(column);
+    await _context.SaveChangesAsync();
+
+    var dto = new CreateTaskDto
+    {
+      Title = "task1",
+
+    };
     // When
+    var task = await _service.CreateTaskAsync(columnId, dto);
+
+    // Then
+    Assert.NotNull(task);
+    Assert.Equal(task.Title, dto.Title);
+    Assert.Equal(task.ColumnId, columnId);
+  }
+
+  [Fact]
+  public async Task GetTaskAsync_ShouldReturnTask_WhenExists()
+  {
+    // Given
+    var expectedId = Guid.NewGuid();
+    var expected = new ProjectTask
+    {
+      Title = "task1",
+      Id = expectedId
+    };
+    _context.ProjectTasks.Add(expected);
+
+    await _context.SaveChangesAsync();
+  
+    // When
+    var task = await _service.GetTaskAsync(expected.Id);
   
     // Then
+    Assert.NotNull(task);
+    Assert.Equal(expectedId, task.Id);
   }
 
   public void Dispose()
