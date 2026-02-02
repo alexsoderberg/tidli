5d2efe5333d76b87cc3d2387fecaf59606253938
Alex Söderberg <as228gc@student.lnu.se>
2026-02-02 05:46:56 +0100

    Test: Add fake project implementation

---
+++ apps/project-service/tests/ProjectService.Tests/Fakes/Services/FakeProjectService.cs
@@ -0,0 +1,29 @@
+using project_service.Dtos;
+using project_service.Models;
+using project_service.Services.Interfaces;
+
+namespace ProjectService.Tests.Fakes.Services;
+
+public class FakeProjectService : IProjectService
+{
+
+  public Project ProjectToReturn { get; set; } = new Project { Name = "FakeProject"};
+  public bool DeleteResult { get; set; } = true;
+  public Project? LastCreatedProject { get; private set; }
+
+
+  public Task<Project> CreateProjectAsync(CreateProjectDTO dto)
+  {
+    return Task.FromResult(ProjectToReturn);
+  }
+
+  public Task<bool> DeleteProjectAsync(Guid id)
+  {
+    return Task.FromResult(DeleteResult);
+  }
+
+  public Task<Project?> GetProjectAsync(Guid id)
+  {
+    return Task.FromResult(LastCreatedProject);
+  }
+}
