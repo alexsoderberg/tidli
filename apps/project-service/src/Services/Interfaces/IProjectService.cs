24805ac89c40fc79dfabc3abbe48aa75162fdfb8
Alex Söderberg <as228gc@student.lnu.se>
2026-02-02 16:53:02 +0100

    Test: Fix CreateColumnAsync test case

--- apps/project-service/src/Services/Interfaces/IProjectService.cs
+++ apps/project-service/src/Services/Interfaces/IProjectService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using project_service.Dtos;
 using project_service.Models;
 
@@ -8,6 +9,6 @@ public interface IProjectService
   Task<Project?> GetProjectAsync(Guid id);
 
   Task<Project> CreateProjectAsync(CreateProjectDTO dto);
-  
+
   Task<bool> DeleteProjectAsync(Guid id);
 }
