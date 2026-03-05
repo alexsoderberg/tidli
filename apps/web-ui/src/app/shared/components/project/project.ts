b9efbfa0af3780c8636e4a290e4ff66357a641af
Alex Söderberg <soderberg.alex@outlook.com>
2026-03-05 23:16:28 +0100

    Feat: Create project component

---
+++ apps/web-ui/src/app/shared/components/project/project.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Stage } from "../stage/stage";
+
+@Component({
+  selector: 'app-project',
+  imports: [Stage],
+  templateUrl: './project.html',
+  styleUrl: './project.css',
+})
+export class Project {
+
+}
